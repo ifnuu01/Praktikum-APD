22e30f9de851a31cf222da3c7eb20237218437db
MatchaTi <tehmatcha28@gmail.com>
2024-09-20 10:49:03 +0800

    feat: init pertama

---
+++ Kelas/Pertemuan-2/main.py
@@ -0,0 +1,10 @@
+# angka = int(input("Masukkan angka: "))
+# result = "Genap" if angka % 2 == 0 else "Ganjil"
+# print(angka, "adalah angka",result)
+
+# cuaca  =input(“masukan cuaca saat ini >> “)
+
+angka = input("Masukkkan angka: ")
+
+angka += 2
+print(angka)
