4bfec6a304501682122eb7b1261de323fad658d3
Adi Muhammad Syifai <118320176+MatchaTi@users.noreply.github.com>
2024-09-19 23:00:12 -0800

    Pertemuan ke - 3 Perkondisian

---
+++ Kelas/pertemuan-3/main.py
@@ -0,0 +1,13 @@
+print("Menu")
+print("1. Nonton film")
+print("2. Ngoding")
+print("3. Keluar")
+menu = int(input("Pilih Menu anda : "))
+if menu==1 :
+    print("sedang menonton Film")
+elif menu==2 : 
+    print("sedang ngoding")
+elif menu==3 : 
+    print("Sedang keluar")
+else:
+    print("Menu tidak tersedia")
---
+++ Kelas/pertemuan-3/perkondisian.py
@@ -0,0 +1,76 @@
+cuaca = "mendung"
+
+# if cuaca == "cerah":
+#     print("Kamu pergi keluar rumah")
+#     print("mahal")
+
+# variable akan bernilai false jika string kosong, 0, [], (), {}, false
+# uang = 20000
+# if uang:
+#     print("Pergi ke Pasar!")
+# else:
+#     print("Pergi ke rumah teman")
+
+# nilai = -1
+# if nilai < 70:
+#     print("tidak lulus")
+# else:
+#     print("lulus")
+
+# nilai = -0
+
+# if nilai < 0:
+#     print("Bilangan negatif")
+# else:
+#     print("Bilangan positif")
+# umur = int(input("Masukkan umur anda: "))
+# if umur <= 10:
+#     print("anak-anak")
+# elif umur <= 18:
+#     print("remaja")
+# elif umur <=35:
+#     print("dewasa")
+# elif umur <=65:
+#     print("paruh baya")
+# else:
+#    print("tua")
+
+# nilai = 79
+# if nilai >= 80 :
+#     if nilai >= 85 :
+#         print("nilai A+")
+#     else :
+#         print("Nilai A-")
+# else :
+#     print("tidak lulus")
+
+# print("Menu")
+# print("1. Nontoh Film")
+# print("2. Ngoding")
+# print("3. Keluar")
+
+# pilihan = input("Masukkan pilihan: ")
+
+# if pilihan == "1":
+#     print("Sedang nonton film")
+# elif pilihan == "2":
+#     print("Sedang ngoding")
+# elif pilihan == "3":
+#     print("Keluar dai aplikasi")
+# else:
+#     print("Menu tidak tersedia")
+
+angka = 10
+# perkondisian pertama
+if angka % 2 == 0:
+    print("Genap")
+if angka > 0:
+    print("positif")
+
+# perkondisian kedua
+if angka % 2 == 0:
+    print("Genap")
+elif angka > 0:
+    print("positif")
+
+# if elif ... else
